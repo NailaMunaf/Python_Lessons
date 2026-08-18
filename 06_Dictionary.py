969e3dfbbd178930ca68671cfba8dca1d19c3dee
NailaMunaf <nailaabdulmanaf1@gmail.com>
2026-08-18 22:08:20 +0500

    Added new example

--- 06_Dictionary.py
+++ 06_Dictionary.py
@@ -53,3 +53,42 @@ dict = {
 }
 print(dict.get("course")) #{'python': 90, 'maths': 100, 'chem': 80}
 print(dict["course"]) #{'python': 90, 'maths': 100, 'chem': 80}
+
+##################################33
+
+def find_student(students, name):
+    for student in students:
+
+        if student["name"] == name:
+
+            total = sum(student["marks"])
+            average = total / len(student["marks"])
+
+            if average >= 90:
+                grade = "A"
+            elif average >= 80:
+                grade = "B"
+            elif average >= 70:
+                grade = "C"
+            else:
+                grade = "D"
+
+            return {
+                "name": student["name"],
+                "total": total,
+                "average": average,
+                "grade": grade
+            }
+
+    return None
+
+
+students = [
+    {"name": "Ali", "marks": [80, 90, 85]},
+    {"name": "Sara", "marks": [95, 92, 98]},
+    {"name": "Ahmed", "marks": [70, 75, 68]}
+]
+
+result = find_student(students, "Sara")
+
+print(result)
